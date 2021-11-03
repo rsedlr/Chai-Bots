60cec02ac684ed73ea5b9a6b99a1fa38d3a385ec
Reiss <30870094+rsedlr@users.noreply.github.com>
2021-11-03 23:30:48 +0000

    final changes

--- RickBot/bot/bot.py
+++ RickBot/bot/bot.py
@@ -1,20 +1,18 @@
 import json
 import requests
 import time
-
 from chai_py import ChaiBot, Update
-
-# TODO:
 
 
 class Bot(ChaiBot):
 
-    contextLength = 5
+    contextLength = 2
 
     def setup(self):
-        self.ENDPOINT = "https://api-inference.huggingface.co/models/rsedlr/RickBot"  # dialogpt output small works :)
+        self.ENDPOINT = "https://api-inference.huggingface.co/models/rsedlr/RickBot"
         self.headers = {
-            "Authorization": "Bearer api_oieZbocfGuGxzuQozzaqpFYnBrpBsSLwzP"
+            # "Authorization": "Bearer api_oieZbocfGuGxzuQozzaqpFYnBrpBsSLwzP"  # old
+            "Authorization": "Bearer api_hcIWnnDcQMnNeJGBIzzQYjorHcYlTEpKXF"
         }
         self.first_response = "Hey, I'm Rick."
 
@@ -27,9 +25,7 @@ class Bot(ChaiBot):
     def query(self, payload):
         data = json.dumps(payload)
         response = requests.post(self.ENDPOINT, headers=self.headers, data=data)
-        if (
-            response.status_code == 503
-        ):  # This means we need to wait for the model to load 😴.
+        if (response.status_code == 503):  # This means we need to wait for the model to load 😴.
             estimated_time = response.json()["estimated_time"]
             time.sleep(estimated_time)
             self.logger.info(f"Sleeping for model to load: {estimated_time}")
@@ -37,14 +33,18 @@ class Bot(ChaiBot):
             data["options"] = {"use_cache": False, "wait_for_model": True}
             data = json.dumps(data)
             response = requests.post(self.ENDPOINT, headers=self.headers, data=data)
+        
+        print(response.content.decode("utf-8"))
         return json.loads(response.content.decode("utf-8"))["generated_text"]
 
     async def get_payload(self, update):
         messages = await self.get_messages(update.conversation_id)
         past_user_inputs = ["Hey"]  # You can change this!
         generated_responses = [self.first_response]
+
         for message in messages:
             content = message.content
+
             if content == self.FIRST_MESSAGE_STRING:
                 continue  # We're not trying to keep track of our FIRST_MESSAGE_STRING (i.e. "__first")
             if message.sender_uid == self.uid:
@@ -53,20 +53,20 @@ class Bot(ChaiBot):
 
                 if len(past_user_inputs) > self.contextLength:
                     past_user_inputs = past_user_inputs[1:]
-                    # del past_user_inputs[: self.contextLength]
-
             else:
                 # Put the model generated messages into here
                 generated_responses.append(content)
 
                 if len(generated_responses) > self.contextLength:
                     generated_responses = generated_responses[1:]
-                    # del generated_responses[: self.contextLength]
+                
+        self.logger.info(f"PastInputs: {past_user_inputs}")
+        self.logger.info(f"Generated Responses: {generated_responses}\n")
 
-            return {
-                "inputs": {
-                    "past_user_inputs": past_user_inputs,
-                    "generated_responses": generated_responses,
-                    "text": update.latest_message.text,
-                },
-            }
+        return {
+            "inputs": {
+                "past_user_inputs": past_user_inputs,
+                "generated_responses": generated_responses,
+                "text": update.latest_message.text,
+            },
+        }
--- deleteBot.py
+++ deleteBot.py
@@ -1,7 +1,7 @@
 from DrunkBot.credentials import DEVELOPER_KEY, DEVELOPER_UID
 from chai_py.deployment import delete_bot
 from chai_py.auth import set_auth
+import sys
 
 set_auth(DEVELOPER_UID, DEVELOPER_KEY)
-
-delete_bot(bot_uid="_bot_00c42083-9c1c-4a07-9092-51719068fd38")
+delete_bot(bot_uid=sys.argv[1])
